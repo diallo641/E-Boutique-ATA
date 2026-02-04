d2e41764cecc25c10a244f7eee4fc605b9ace5ef
Boubacar DIALLO <boubacar95@outlook.fr>
2026-02-04 12:32:27 +0000

    création du fichier pour gerer la connexion avec la base de donnée

---
+++ Backend/config/db.js
@@ -0,0 +1,23 @@
+// config/db.js
+
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+// Charger les variables d'environnement depuis .env
+dotenv.config();
+
+// Création de la connexion
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,        
+  user: process.env.DB_USER,        
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,    
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// Exporter une version promise pour utiliser async/await
+const db = pool.promise();
+
+module.exports = db;
