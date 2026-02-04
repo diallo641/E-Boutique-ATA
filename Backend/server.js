8bd1ecf29a324008abe77b571da87227837bcb72
Boubacar DIALLO <boubacar95@outlook.fr>
2026-02-04 13:11:29 +0000

    Mettre à jour le chemin pour lire le fichier .env

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+// server.js
+
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const db = require('./config/db'); 
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Route test
+app.get("/", (req, res) => {
+  res.send("✅ API fonctionne !");
+});
+
+// Test connexion à la base au démarrage
+db.getConnection()
+  .then(() => console.log("✅ Connecté à MySQL"))
+  .catch(err => console.error("❌ Erreur de connexion MySQL:", err));
+
+// Définir le port
+const port = process.env.PORT || 3000;
+
+// Démarrage du serveur
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Le serveur API a démarré avec succès sur le port ${port}`);
+});
