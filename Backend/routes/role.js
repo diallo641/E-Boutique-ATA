08cff612f65b1a113aa98301eea5f440893d0c4d
Boubacar DIALLO <boubacar95@outlook.fr>
2026-02-05 15:32:56 +0000

    Fichier route pour role

--- Backend/routes/role.js
+++ Backend/routes/role.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const roleController = require('../controllers/roleController');
+const roleController = require('../controllers/role');
 
 //ajouter un role
 router.post('/ajouterrole', roleController.createRole);
