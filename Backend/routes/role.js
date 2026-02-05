e0311e55923e8a8d212e0a3643e6d299b24f765d
Boubacar DIALLO <boubacar95@outlook.fr>
2026-02-05 11:25:44 +0000

    Création des routes pour y acceder sur les plateformes de teste

---
+++ Backend/routes/role.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const roleController = require('../controllers/roleController');
+
+//ajouter un role
+router.post('/ajouterrole', roleController.createRole);
+//avoir tous les rôles
+router.get('/getallroles', roleController.getAllRoles);
+//avoir un rôle par son id
+router.get('/unrole/:id', roleController.getRoleById);
+//editer un rôle
+router.put('/editerrole/:id', roleController.updateRole);
+//supprimer un rôle
+router.delete('/deleterole/:id', roleController.deleteRole);
+
+module.exports = router;
+
