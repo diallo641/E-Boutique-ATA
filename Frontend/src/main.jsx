f46c161382356de4dc32f31ed67cb362e691889b
Boubacar DIALLO <boubacar95@outlook.fr>
2026-03-09 16:26:00 +0000

    Enlever le footer du fichier main

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,10 @@ import './index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import Footer from './composants/footer.jsx';
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <Footer />
   </React.StrictMode>
 );
