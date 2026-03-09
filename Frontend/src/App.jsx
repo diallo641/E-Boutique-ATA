8278ef8ba025b40691a9fe63afecaafae1fe261a
Boubacar DIALLO <boubacar95@outlook.fr>
2026-03-09 16:25:44 +0000

    Création de route de redirection

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,23 @@
-import Home from "./pages/Home";
-import Service from "./pages/services";
-import Activite from "./pages/activites";
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./composants/Headers";
+import Home from "./pages/Accueil/Home"; 
+import Produits from "./pages/Accueil/Produits";
+import Apropos from "./pages/Accueil/Apropos";
+import Contact from "./pages/Accueil/contact";
+import Connexion from "./pages/Authentification/Connexion"
+
+function App() {
   return (
-    <div className="min-h-screen bg-blue-200 flex items-center justify-center">
-      <Home />
-      <Service />
-      <Activite />
-    </div> 
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/produits" element={<Produits />} />
+        <Route path="/apropos" element={<Apropos />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Connexion" element={<Connexion />} />
+      </Routes>
+    </Router>
   );
 }
 
+export default App;
