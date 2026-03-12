3601a211e5abdaabe4f4ef41b7cea7b758d187eb
Boubacar DIALLO <boubacar95@outlook.fr>
2026-03-12 09:40:35 +0000

    Mises à jour du fichier app pour ajouter un boutton

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./composants/Headers";
 import Home from "./pages/Accueil/Home"; 
 import Produits from "./pages/Accueil/Produits";
 import Apropos from "./pages/Accueil/Apropos";
 import Contact from "./pages/Accueil/contact";
-import Connexion from "./pages/Authentification/Connexion"
+import Connexion from "./pages/Authentification/Connexion";
+import Inscription from "./pages/Authentification/Inscription";
+import Reinitialiser_password from "./pages/Authentification/Reinitialiser_password";
+import Panier from "./pages/Accueil/Panier"
 
 function App() {
   return (
@@ -15,6 +17,9 @@ function App() {
         <Route path="/apropos" element={<Apropos />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/Connexion" element={<Connexion />} />
+        <Route path="/Inscription" element={<Inscription />} />
+        <Route path="/Reinitialiser_password" element={<Reinitialiser_password />} />
+        <Route path="/Panier" element={<Panier />} />
       </Routes>
     </Router>
   );
